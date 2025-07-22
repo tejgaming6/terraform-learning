763694a90713a8da3ce1da79ac467a74dceac525
teja <94366226+tejgaming6@users.noreply.github.com>
2025-07-22 13:56:30 +0000

    new changess

--- local.tf
+++ local.tf
@@ -1,5 +1,16 @@
-resource "local_file" "copy" {
-  filename = "ra.txt"
-  content = ""
-  source = "workspaces/terraform-learning/learning/provider.tf"
+resource "local_file" "contet-insert" {
+  filename = "content_insert.txt"
+  content = "I'm passing this info into the file content_insert as soon it's made" 
+}
+resource "local_file" "filecreation11" {
+  filename = "file11.txt"
+  content_base64 = "xyzabc" 
+}
+resource "local_file" "filecreation12" {
+  filename = "file12.txt"
+  content_base64 = "xyzabcefghi" 
+}
+resource "local_file" "source_file_copy11" {
+  filename = "copy11.txt"
+  source = "/workspaces/terraform-learning/learning/local.txt"
 }
